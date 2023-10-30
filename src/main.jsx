f7bfcab0a382b8e92f804c92b3f8b1df774d12b6
Fiap97 <felipearavena63@gmail.com>
2023-10-30 16:14:09 -0300

    Arreglamiento app a App

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import  { App } from './app.jsx'
+import  { App } from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  } from '@fortawesome/free-solid-svg-icons'
 
 
