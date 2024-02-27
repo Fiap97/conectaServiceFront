dde33ff8e569372ecab0db7a292a75f2d353be8f
Fiap97 <felipearavena63@gmail.com>
2024-02-27 14:26:59 -0300

    Avance proyecto sin logica front

---
+++ src/Componentes/ImgUsuario.jsx
@@ -0,0 +1,5 @@
+export function ImgUsuario () {
+    return(
+        <img className="m-2 h-14 w-14 rounded-full" alt="el avatar de triple h" src="https://pbs.twimg.com/profile_images/1645908612458348546/y3FPlRob_400x400.jpg"/>    
+    )
+}
